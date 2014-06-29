66e625e246ea7fc585b82953250cb1144f44fed1
Nickolai Novik <is.infinity@yahoo.com>
2014-06-29 22:40:30 +0300

    fix example

--- examples/simple.py
+++ examples/simple.py
@@ -2,12 +2,18 @@ import asyncio
 import aiomcache
 
 
-@asyncio.corotine
+loop = asyncio.get_event_loop()
+
+
+@asyncio.coroutine
 def hello_aiomcache():
-    mc = aiomcache.Client("127.0.0.1", 11211)
+    mc = aiomcache.Client("127.0.0.1", 11211, loop=loop)
     yield from mc.set(b"some_key", b"Some value")
     value = yield from mc.get(b"some_key")
     print(value)
     values = yield from mc.multi_get(b"some_key", b"other_key")
     print(values)
     yield from mc.delete(b"another_key")
+
+
+loop.run_until_complete(hello_aiomcache())
