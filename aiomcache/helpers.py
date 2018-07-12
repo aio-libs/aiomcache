792ccd78772d803539ea898ddfbdfa520b1297d3
Alexander Mohr <thehesiod@hotmail.com>
2018-07-11 17:39:04 -0700

    add pylibmc helper

---
+++ aiomcache/helpers.py
@@ -0,0 +1,21 @@
+from enum import IntEnum
+import pickle
+
+
+class PyLibMCFlags(IntEnum):
+    PYLIBMC_FLAG_PICKLE = 1 << 0
+    PYLIBMC_FLAG_INTEGER = 1 << 1
+    PYLIBMC_FLAG_LONG = 1 << 2
+    PYLIBMC_FLAG_BOOL = 1 << 4
+
+
+# see: https://github.com/lericson/pylibmc/blob/master/src/_pylibmcmodule.c#L640 (_PylibMC_deserialize_native)
+async def pylibmc_flag_handler(value, flags):
+    if flags == PyLibMCFlags.PYLIBMC_FLAG_PICKLE:
+        return pickle.loads(value)
+    elif flags == PyLibMCFlags.PYLIBMC_FLAG_LONG:
+        return int(value)
+    elif flags == PyLibMCFlags.PYLIBMC_FLAG_BOOL:
+        return bool(int(value))
+    else:
+        assert False
