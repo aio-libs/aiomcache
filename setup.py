bbe48833c508afa07d83dd4e3682f58e200b542e
Sam Bull <git@sambull.org>
2022-01-16 18:34:44 +0000

    Fix

--- setup.py
+++ setup.py
@@ -8,8 +8,7 @@ from setuptools import find_packages, setup
 with codecs.open(os.path.join(os.path.abspath(os.path.dirname(
         __file__)), 'aiomcache', '__init__.py'), 'r', 'latin1') as fp:
     try:
-        version = re.findall(r"^__version__ = '([^']+)'\r?$",
-                             fp.read(), re.M)[0]
+        version = re.findall(r'^__version__ = "([^"]+)"\r?$', fp.read(), re.M)[0]
     except IndexError:
         raise RuntimeError('Unable to determine version.')
 
