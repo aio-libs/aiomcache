f1e03a3ebfacb49f192be3c386965dc5f5503d68
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-06-09 18:03:31 +0400

    change formating on % literals, more readable

--- aiomcache/client.py
+++ aiomcache/client.py
@@ -310,9 +310,9 @@ class FlagClient(Generic[_T]):
                 raise ValidationException("flag handler must be set for non-byte values")
             value, flags = await self._set_flag_handler(value)
 
-        _cmd = command + b' ' + key + f"{flags} {exptime} {len(value)}".encode()
+        _cmd = b'%s %s %s' % (command,  key, f'{flags} {exptime} {len(value)}'.encode())
         if cas:
-            _cmd += b' ' + f"{cas}".encode()
+            _cmd += b' ' + f'{cas}'.encode()
         cmd = _cmd + b'\r\n' + value + b'\r\n'
         resp = await self._execute_simple_command(conn, cmd)
 
