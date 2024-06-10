52dd765b090f646f47d7dcbf1ab7323b46b2e562
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-06-11 00:26:17 +0400

    changes to config `max-line-length = 90`

--- aiomcache/client.py
+++ aiomcache/client.py
@@ -310,7 +310,9 @@ class FlagClient(Generic[_T]):
                 raise ValidationException("flag handler must be set for non-byte values")
             value, flags = await self._set_flag_handler(value)
         cas_value = b" %a" % cas if cas else b""
-        cmd = b"%b %b %a %a %a%b\r\n%b\r\n" % (command, key, flags, exptime, len(value), cas_value, value)
+        cmd = b"%b %b %a %a %a%b\r\n%b\r\n" % (
+            command, key, flags, exptime, len(value), cas_value, value
+        )
         resp = await self._execute_simple_command(conn, cmd)
 
         if resp not in (
